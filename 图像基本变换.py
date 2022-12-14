6cf9f7941ae14e6053d2cc11684278a20143826e
SiriusBad <wyzsirius@icloud.com>
2022-12-14 18:23:23 +0800

    sirius

--- 图像基本变换.py
+++ 图像基本变换.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 dog=cv2.imread('dog.jpg')
 
@@ -55,3 +56,68 @@ new_dog = cv2.rotate(dog, rotateCode=cv2.ROTATE_90_COUNTERCLOCKWISE)
 
 
 """仿射变换"""
+
+"""仿射变换是图像旋转, 缩放, 平移的总称.具体的做法是通过一个矩阵和和原图片坐标进行计算, 得到新的坐标, 完成变换. 所以关键就是这个矩阵.
+
+warpAffine(src, M, dsize, flags, mode, value)
+
+M:变换矩阵
+
+dsize: 输出图片大小
+
+flag: 与resize中的插值算法一致
+
+mode: 边界外推法标志
+
+value: 填充边界值
+"""
+
+"""getRotationMatrix2D(center, angle, scale)
+    center 中心点 , 以图片的哪个点作为旋转时的中心点.
+    angle 角度: 旋转的角度, 按照逆时针旋转.
+    scale 缩放比例: 想把图片进行什么样的缩放."""
+
+dog=cv2.imread('dog.jpg')
+#获取原始图片的大小
+h,w,ch=dog.shape
+#获取变换矩阵 以点(300,480)为中心 旋转 15度 缩放比例为1
+# print(h,w,ch)
+M=cv2.getRotationMatrix2D((300,480),15,1)
+#仿射变化
+dog=cv2.warpAffine(dog,M,dsize=(w,h))
+cv2.imshow('img',dog)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+"""getAffineTransform(src[], dst[]) 通过三点可以确定变换后的位置, 相当于解方程, 3个点对应三个方程, 能解出偏移的参数和旋转的角度.
+    src原目标的三个点
+    dst对应变换后的三个点"""
+# 通过三个点来确定M
+# 仿射变换之平移
+dog=cv2.imread('dog.jpg')
+h, w, ch = dog.shape
+
+# 一般是横向和纵向的点, 所以一定会有2个点横坐标相同, 2个点纵坐标相同
+src = np.float32([[200, 100], [300, 100], [200, 300]])
+dst = np.float32([[100, 150], [360, 200], [280, 120]])
+M = cv2.getAffineTransform(src, dst)
+# 注意opencv中是先宽度, 再高度
+new = cv2.warpAffine(dog, M, (w, h))
+
+cv2.imshow('new', new)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+"""透视变换就是将一种坐标系变换成另一种坐标系. 简单来说可以把一张"斜"的图变"正",或者将一张 ‘正’ 图片 变 ‘斜’"""
+dog=cv2.imread('dog.jpg')
+print(dog.shape)
+#获取变换矩阵
+ori_point=np.float32([[10,10],[210,110],[0,350],[250,390]])
+res_point=np.float32([[20,15],[230,110],[20,300],[300,350]])
+M=cv2.getPerspectiveTransform(ori_point,res_point)
+
+img2=cv2.warpPerspective(dog,M,dsize=(640,480))
+cv2.imshow('img',img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
