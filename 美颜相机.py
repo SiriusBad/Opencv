e756db7569047662bd4d065d1c15b2285a802024
SiriusBad <wyzsirius@icloud.com>
2022-12-17 17:35:45 +0800

    sirius

--- 美颜相机.py
+++ 美颜相机.py
@@ -8,14 +8,9 @@ import numpy as np
 cap = cv2.VideoCapture(1)
 
 while True:
-    """读每一帧，返回标记和这一帧数据"""
     ret,frame = cap.read()
-
-    """根据ret做判断"""
     if not ret:
         break
-
-    """反转"""
     frame = cv2.flip(frame, 1)
     dst = cv2.bilateralFilter(frame, 9, 20, 50)
     cv2.imshow('sirius',np.hstack((frame, dst)))
