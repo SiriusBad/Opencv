8309a849d1cac87e2418da5ce35b42d5ed7947bc
SiriusBad <wyzsirius@icloud.com>
2022-12-19 20:49:00 +0800

    sirius

--- Practice/ Face Recognition/Face Recognition Plus.py
+++ Practice/ Face Recognition/Face Recognition Plus.py
@@ -67,13 +67,13 @@ while True:
         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
         matchIndex = np.argmin(faceDis)
 #
-        if matches[matchIndex] and faceDis[matchIndex]<0.5:
+        if matches[matchIndex] and faceDis[matchIndex]<0.4:
             name = classNames[matchIndex]
             y1, x2, y2, x1 = faceLoc
             # y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
             # cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, name+str(faceDis[matchIndex]), (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
         else:
             name = 'Unknown'
             y1, x2, y2, x1 = faceLoc
